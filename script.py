c91e5c8862ab7d331344012b7d9c09cf1f6143e7
高明昊 <gaoryrt@baicizhan.com>
2024-02-26 17:24:00 +0800

    feat: ✨ new cron, new log

--- script.py
+++ script.py
@@ -95,15 +95,9 @@ def main():
     afternoon_webhook_url = os.getenv("AFTERNOON_WEBHOOK_URL")
     print(beforenoon_webhook_url,afternoon_webhook_url,os.getenv("GEMINI_API_KEY"))
     print("script is runnnig!")
-    if is_time_in_window("10:45"):
-        message = get_generated_message()
-        send_message_to_webhook(beforenoon_webhook_url, message)
-    elif is_time_in_window("14:22") or is_time_in_window("15:45") or is_time_in_window("17:07"):
-        message = get_generated_message()
-        send_message_to_webhook(afternoon_webhook_url, message)
-    elif is_time_in_window("18:00"):
-        message = get_frank_message()
-        send_message_to_webhook(afternoon_webhook_url,gen_message(message))
+    message = get_generated_message()
+    print(message)
+    send_message_to_webhook(beforenoon_webhook_url, message)
 
 if __name__ == "__main__":
     main()
